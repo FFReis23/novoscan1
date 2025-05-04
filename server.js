9d7e62cc4506ecf525ca0791cadb5a8c3862a9e5
FFReis23 <machshev.reis@gmail.com>
2025-05-04 11:57:02 -0300

    Atualiza configurações e adiciona novos arquivos

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7757093107:AAHFmLastbU0lkV5f2AdeiNXHoxckQsJVy4"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "1276935257"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
